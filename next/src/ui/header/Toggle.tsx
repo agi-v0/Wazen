25119a8587e0a05c160988ab3a799637ddbfa614
Ghaith <ghaithatrk93@gmail.com>
2024-05-29 02:06:41 +0800

    navbar menu icon adjustment

--- next/src/ui/header/Toggle.tsx
+++ next/src/ui/header/Toggle.tsx
@@ -2,7 +2,7 @@ import { AiOutlineMenu } from 'react-icons/ai'
 
 export default function Toggle() {
 	return (
-		<label className="[grid-area:toggle] md:hidden">
+		<label className="[grid-area:toggle] md:hidden mt-1">
 			<input id="header-open" type="checkbox" hidden />
 			<span className="header-closed:hidden">
 				<AiOutlineMenu />
