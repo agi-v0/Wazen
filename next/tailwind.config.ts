f3318766682eeb9ffc8be294f030cb6d4c2e08e7
Ghaith <ghaithatrk93@gmail.com>
2024-06-09 14:49:43 +0800

    tailwind config adjustment

--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -1,9 +1,11 @@
 import plugin from 'tailwindcss/plugin'
+import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{ts,tsx}"],
-	darkMode: "class",
+const config: Config = {
+	content: [
+		'./src/{app,ui}/**/*.{ts,tsx}',
+		"./src/**/*.{ts,tsx}"
+	],
 	theme: {
 		extend: {
 			colors: {
@@ -17,22 +19,23 @@ module.exports = {
 				fold: 'calc(100svh - var(--header-height))',
 			},
 			animation: {
-        scroll:
-          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-      },
-      keyframes: {
-        scroll: {
-          to: {
-            transform: "translate(calc(-50% - 0.5rem))",
-          },
-        },
-      },
+				scroll:
+					"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+			},
+			keyframes: {
+				scroll: {
+					to: {
+						transform: "translate(calc(-50% - 0.5rem))",
+					},
+				},
+			},
 		},
-		plugins: [
-			plugin(function ({ addVariant }) {
-				addVariant('header-closed', 'body:has(#header-open:not(:checked)) &')
-			}),
-		],
-		safelist: ['action'],
 	},
-};
+	plugins: [
+		plugin(function ({ addVariant }) {
+			addVariant('header-closed', 'body:has(#header-open:not(:checked)) &')
+		}),
+	],
+	safelist: ['action'],
+}
+export default config
