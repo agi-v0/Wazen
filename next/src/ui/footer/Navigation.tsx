4b20624f1987d49b7d449fdf8ee1b0a778ed834a
omaro <omaroubari16@gmail.com>
2024-06-10 14:00:27 +0300

    slight footer edits

--- next/src/ui/footer/Navigation.tsx
+++ next/src/ui/footer/Navigation.tsx
@@ -8,16 +8,21 @@ export default async function Menu({ footerMenu }: any) {
 				const { label, links } = item
 
 				return (
-					<div key={key} className="cursor-default md:w-1/3 lg:w-fit ">
-						<div className="mb-4 h-8 rounded text-start ">{label}</div>
+					<div key={key} className="cursor-default md:w-1/3 lg:w-fit">
+						<div className="mb-4 rounded text-start font-medium text-teal-50">
+							{label}
+						</div>
 
 						<ul className="text-start">
 							{links?.map((link: any, key: any) => (
 								<li
 									key={key}
-									className="group flex h-8 items-center text-gray-50/50"
+									className="group flex h-8 items-center text-teal-50/80"
 								>
-									<Link href="" className="no-underline group-hover:text-white">
+									<Link
+										href=""
+										className="no-underline group-hover:text-teal-100"
+									>
 										{link.label}
 									</Link>
 									<PiCaretDownBold className="size-3 translate-x-0 text-gray-50/50 opacity-0 transition-transform duration-300 group-hover:-translate-x-[4px] group-hover:opacity-100 md:rotate-90" />
