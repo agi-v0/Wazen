f58cbafba7cad969f86ee333e44fd5c48f1f506f
Ghaith <ghaithatrk93@gmail.com>
2024-06-23 05:40:49 +0800

    how it works adjustment

--- next/src/ui/modules/HowItWorks.tsx
+++ next/src/ui/modules/HowItWorks.tsx
@@ -39,22 +39,22 @@ export default function HowItWorks({
 
 	return (
 		<section className={'section py-12'}>
-			<div className="fluid-gap flex w-full flex-col items-center justify-evenly gap-y-6 rounded-sm border-8 border-white bg-teal-50 p-6 text-teal-600 shadow-md">
-				<div className="flex max-w-2xl flex-col gap-4">
+			<div className="fluid-gap flex w-full flex-col items-center justify-evenly gap-y-6 rounded-xl border-8 border-white bg-teal-50 p-6 text-teal-600 shadow-md">
+				<div className="my-6">
 					<PortableText value={content} components={components} />
 				</div>
 				<ul className="flex flex-col justify-evenly w-full md:flex-row">
 					{steps?.map((step, index) => (
 						<li
-							className="flex flex-col items-center justify-center gap-4 mb-3"
+							className="flex flex-col items-center justify-center gap-4"
 							key={index}
 						>
 							<Img
 								image={step.image}
-								imageWidth={200}
-								className="rounded-xl"
+								imageWidth={224}
+								className="rounded-2xl border-8 border-teal-100"
 							/>
-							<div>{step.text}</div>
+							<div className='text-2xl my-4 font-bold'>{step.text}</div>
 						</li>
 					))}
 				</ul>
