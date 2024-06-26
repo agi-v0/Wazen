7b0804108c79e99edc3b70be311be68aa9241d64
Ghaith <ghaithatrk93@gmail.com>
2024-06-26 19:52:25 +0800

    post adjustment

--- next/src/ui/modules/blog/PostPreview.tsx
+++ next/src/ui/modules/blog/PostPreview.tsx
@@ -7,45 +7,40 @@ import Img from '@/ui/Img'
 export default function PostPreview({ post }: { post: Sanity.BlogPost }) {
 	return (
 		<Link
-			className="group block"
 			href={`/blog/${post.metadata?.slug?.current}`}
 		>
-			<div className="w-full rounded-md object-cover p-2 shadow-md">
+			<div className="w-full rounded-2xl p-2 shadow-md md:min-h-[450px]">
 				{post.metadata.image ? (
 					<Img
 						image={post.metadata.image}
 						imageWidth={600}
-						className="w-full"
+						className="w-full rounded-md"
 					/>
 				) : (
-						<Image
-							src="/image-placeholder.jpg"
-							alt=""
-							width={600}
-							height={190}
-							className='h-[190px] object-cover'
-						/>
+					<Image
+						src="/image-placeholder.jpg"
+						alt=""
+						width={600}
+						height={190}
+						className="h-[190px] rounded-md object-cover"
+					/>
 				)}
-				<div className="flex gap-4">
-					{post?.categories && (
-						<div className="my-2 w-fit rounded-full border border-[#0D9488] bg-[#0D9488]/20 px-6 py-1 text-[#0D9488]">
-							{post?.categories[0]?.title}
+				<div className='flex flex-col space-between h-full'>
+					<div className="flex gap-2">
+						{post?.categories && (
+							<div className="my-2 w-fit rounded-full border-2 border-[#0D9488]/20 p-2 text-sm text-[#0D9488]">
+								{post?.categories[0]?.title}
+							</div>
+						)}
+						<div className="my-2 w-fit rounded-full bg-[#0D9488]/20 p-2 text-[#0D9488]">
+							<Date value={post.publishDate} />
 						</div>
-					)}
-					<div className="my-2 w-fit rounded-full border border-[#0D9488] bg-[#0D9488]/20 px-6 py-1 text-[#0D9488]">
-						<Date value={post.publishDate} />
 					</div>
-				</div>
-				<div className="w-[90%]">
 					<div className="my-4 text-2xl group-hover:underline">
 						{post.title}
 					</div>
-					<div className="text-md my-4">
-						{post?.body[0]?.children[0] &&
-							post.body[0].children[0].text.slice(0, 150) + '...'}
-					</div>
+					<div className="text-[#0D9488]">إقرأ المزيد</div>
 				</div>
-				<div className="text-[#0D9488]">إقرأ المزيد</div>
 			</div>
 		</Link>
 	)
