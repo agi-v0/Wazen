d60c769a7c57fa03bf32cb6277f4c192169dda1b
Ghaith <ghaithatrk93@gmail.com>
2024-07-06 04:42:00 +0800

    language dropdown adjustment

--- next/src/components/lang-select.tsx
+++ next/src/components/lang-select.tsx
@@ -1,13 +1,41 @@
-import { PiGlobe } from 'react-icons/pi'
+'use client'
 
-function LangSelect() {
+import { PiCaretRightBold, PiGlobe } from 'react-icons/pi'
+import InteractiveDetails from '@/ui/header/InteractiveDetails'
+import { useRouter } from 'next/navigation'
+
+function LangSelect({ locale }: any) {
+	const Router = useRouter()
+
+	const changeLanguage = (lang: string) => {
+		const newLocale = lang
+		Router.push(`/${newLocale}`)
+	}
+
 	return (
 		<div className="flex items-center">
-			<PiGlobe />
-			<select>
-				<option value="ar">Arabic</option>
-				<option value="en">English</option>
-			</select>
+			<InteractiveDetails className="group relative">
+				<summary className="flex h-8 items-center gap-1 rounded px-3 no-underline hover:bg-gray-500/5 hover:text-teal-600">
+					<PiGlobe />
+					{locale == 'ar' ? 'Arabic' : 'English'}
+					<PiCaretRightBold className="size-3 translate-y-0 text-gray-500/50 transition-transform duration-300 group-open:rotate-90 group-hover:translate-y-[2px] md:rotate-90" />
+				</summary>
+
+				<ul className="anim-fade-to-b top-full flex flex-col justify-between gap-1 rounded-lg border border-gray-100 bg-white p-3 shadow-md md:absolute">
+					<li
+						className="cursor-pointer px-4 py-2 hover:bg-gray-500/5"
+						onClick={() => changeLanguage('ar')}
+					>
+						Arabic
+					</li>
+					<li
+						className="cursor-pointer px-4 py-2 hover:bg-gray-500/5"
+						onClick={() => changeLanguage('en')}
+					>
+						English
+					</li>
+				</ul>
+			</InteractiveDetails>
 		</div>
 	)
 }
