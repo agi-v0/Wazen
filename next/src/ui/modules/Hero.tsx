b8957a7277d6800ff3c84f89a21876acb1012f83
omaro <omaroubari16@gmail.com>
2024-08-04 03:57:23 +0300

    small hero fix

--- next/src/ui/modules/Hero.tsx
+++ next/src/ui/modules/Hero.tsx
@@ -39,14 +39,14 @@ export default function Hero({
 												radial-gradient(ellipse at top center, #155e75 0%, #2dd4bf 60%, #f0fdfa 100%)`,
 			}}
 		>
-			<div className="absolute top-0 h-full w-full">
-				{/* <Image
+			{/* <div className="absolute top-0 h-full w-full">
+				<Image
 					src={lightray}
 					alt="hero"
 					className="pointer-events-none fixed left-0 right-0 top-0 mx-auto h-screen w-full object-cover mix-blend-overlay"
 					draggable={false}
-				/> */}
-			</div>
+				/>
+			</div> */}
 			<div className="section relative flex w-full flex-col justify-center">
 				<div
 					className={cn('richtext relative space-y-6 pt-[25vh] text-white')}
