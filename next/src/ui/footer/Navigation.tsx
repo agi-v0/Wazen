2706290a305cd1fd67dadb10003ead52c4dd9f57
omaro <omaroubari16@gmail.com>
2024-08-05 04:17:17 +0300

    test: revalidate server action

--- next/src/ui/footer/Navigation.tsx
+++ next/src/ui/footer/Navigation.tsx
@@ -18,9 +18,7 @@ export default async function Menu({ footerMenu, locale }: any) {
 				const { label, links } = item
 				return (
 					<div key={key}>
-						<div className="mb-4 h-9 rounded text-start text-white/50">
-							{label}
-						</div>
+						<p className="mb-4 h-9 rounded text-start text-white/50">{label}</p>
 						<ul className="text-start">
 							{links?.map((link: any, key: any) => (
 								<li
