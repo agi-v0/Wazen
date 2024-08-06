b9de173d1810042720075672f931021e9462645d
omaro <omaroubari16@gmail.com>
2024-08-06 19:30:03 +0300

    fixed quantity state

--- next/src/components/calculator/BodySection.tsx
+++ next/src/components/calculator/BodySection.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useAppDispatch, useAppSelector } from '@/store/hooks'
+// import { useAppDispatch, useAppSelector } from '@/store/hooks'
 import React, { useState } from 'react'
 import {
 	PiFlowArrowDuotone,
@@ -8,7 +8,7 @@ import {
 	PiTableDuotone,
 	PiUserListDuotone,
 } from 'react-icons/pi'
-import { changeQuantity } from '@/store/calculator'
+// import { changeQuantity } from '@/store/calculator'
 
 const BodySection = ({ details }: any) => {
 	const AppIcons = [
@@ -18,93 +18,143 @@ const BodySection = ({ details }: any) => {
 		<PiFlowArrowDuotone className="text-2xl text-teal-500" />,
 	]
 
-	const [quantity, setQuantity] = useState(1)
+	const [quantities, setQuantities] = useState<{ [key: string]: number }>({})
 
-	const dispatch = useAppDispatch()
+	// const dispatch = useAppDispatch()
 
 	const handleMinus = (id: any) => {
-		dispatch(changeQuantity({ id: id, quantity: quantity - 1 }))
-		setQuantity(quantity - 1 < 1 ? 1 : quantity - 1)
+		setQuantities((prevQuantities: { [id: string]: number }) => ({
+			...prevQuantities,
+			[id]: Math.max(0, (prevQuantities[id] || 0) - 1),
+		}))
 	}
 
 	const handlePlus = (id: any) => {
-		dispatch(changeQuantity({ id: id, quantity: quantity + 1 }))
-		setQuantity(quantity + 1)
+		setQuantities((prevQuantities: { [id: string]: number }) => ({
+			...prevQuantities,
+			[id]: (prevQuantities[id] || 0) + 1,
+		}))
 	}
 
 	return (
-		<div>
-			{details?.map((detail: any, index: any) => (
-				<div key={'details_' + index}>
-					<div
-						className="flex flex-row justify-start gap-2 rounded-md bg-teal-50 px-6 py-2 max-md:hidden md:order-1"
-						aria-hidden="true"
-					>
-						{AppIcons[index]}
-						{detail.title}
-					</div>
-					<div className="py-2 text-gray-600">
-						{detail.specs?.rows?.map((row: any, index: any) => {
-							if (typeof window !== 'undefined') {
-								localStorage.setItem(
-									'details',
-									JSON.stringify(detail.specs.rows),
-								)
-							}
-							return (
-								<div
-									key={'rows_' + index}
-									className="mb-2 flex flex-row justify-between"
-								>
-									{row.cells.slice(0, 1).map((cell: any, index: any) => (
+		<>
+			{details?.map((detail: { title: string; specs: any }, index: any) => {
+				console.log('rows', detail.specs.rows)
+				return (
+					<div key={'details_' + index}>
+						<div
+							className="flex flex-row justify-start gap-2 rounded-md bg-teal-50 px-6 py-3 max-md:hidden md:order-1"
+							aria-hidden="true"
+						>
+							{AppIcons[index]}
+							{detail.title}
+						</div>
+						<div className="py-2 text-gray-600">
+							{detail.specs?.rows?.map(
+								(row: { cells: string[] }, _key: string) => {
+									const rowKey = `${_key}`
+									const quantity = quantities[rowKey] || 0
+
+									return (
 										<div
-											key={'cell_' + index}
-											className="flex w-full flex-wrap justify-start px-6 py-2"
+											key={_key}
+											id={_key}
+											className="mb-2 flex flex-row items-center justify-between"
 										>
-											{cell}
-										</div>
-									))}
-									{row.cells.slice(1, 2).map((cell: any, index: any) => (
-										<div
-											key={'cell_' + index}
-											className="flex w-full flex-wrap justify-start px-6 py-2"
-										>
-											{cell} ريال / مندوب / سنويا
-										</div>
-									))}
-									<div className="w-full">
-										<div className="flex h-full flex-row items-center justify-center gap-x-2">
-											<button
-												className="rounded-full px-2 hover:bg-gray-50"
-												onClick={() => handlePlus(row._key)}
+											<div
+												key={'cell-title' + _key}
+												className="flex w-full flex-wrap justify-start px-6 py-2"
 											>
-												+
-											</button>
-											<div className="px-2">{quantity}</div>
-											<button
-												className="rounded-full px-2 hover:bg-gray-50"
-												onClick={() => handleMinus(row._key)}
+												{row.cells[0]}
+											</div>
+											<div
+												key={'cell-value' + _key}
+												className="flex w-full flex-wrap justify-start px-6 py-2"
 											>
-												-
-											</button>
-										</div>
-									</div>
-									{row.cells.slice(1, 2).map((cell: any, index: any) => (
+												{row.cells[1]} ريال / مندوب / سنويا
+											</div>
+											<div className="flex h-full flex-row items-center justify-center gap-x-2">
+												<button
+													className="rounded-full px-2 hover:bg-gray-50"
+													onClick={() => handlePlus(_key)}
+												>
+													+
+												</button>
+												<div className="px-2">{quantity}</div>
+												<button
+													className="rounded-full px-2 hover:bg-gray-50"
+													onClick={() => handleMinus(_key)}
+												>
+													-
+												</button>
+											</div>
+											{/* {row.cells.slice(1, 2).map((cell: any, index: any) => (
 										<div
 											key={'cell_' + index}
 											className="flex w-full flex-wrap justify-start px-6 py-2"
 										>
 											{parseInt(cell) * quantity} ريال / مندوب / سنويا
 										</div>
-									))}
-								</div>
-							)
-						})}
+									))} */}
+										</div>
+									)
+								},
+							)}
+						</div>
 					</div>
-				</div>
-			))}
-		</div>
+				)
+			})}
+		</>
 	)
 }
 
 export default BodySection
+
+// <div
+// 											key={'rows_' + index}
+// 											className="mb-2 flex flex-row justify-between"
+// 										>
+// 											{row.cells.slice(0, 1).map((cell: any, index: any) => {
+// 												return (
+// 													<div
+// 														key={'cell_' + index}
+// 														className="flex w-full flex-wrap justify-start px-6 py-2"
+// 													>
+// 														{cell}
+// 													</div>
+// 												)
+// 											})}
+// 											{row.cells.slice(1, 2).map((cell: any, index: any) => (
+// 												<div
+// 													key={'cell_' + index}
+// 													className="flex w-full flex-wrap justify-start px-6 py-2"
+// 												>
+// 													{cell} ريال / مندوب / سنويا
+// 												</div>
+// 											))}
+// 											<div className="w-full">
+// 												<div className="flex h-full flex-row items-center justify-center gap-x-2">
+// 													<button
+// 														className="rounded-full px-2 hover:bg-gray-50"
+// 														onClick={() => handlePlus(row._key)}
+// 													>
+// 														+
+// 													</button>
+// 													<div className="px-2">{quantity}</div>
+// 													<button
+// 														className="rounded-full px-2 hover:bg-gray-50"
+// 														onClick={() => handleMinus(row._key)}
+// 													>
+// 														-
+// 													</button>
+// 												</div>
+// 											</div>
+// 											{row.cells.slice(1, 2).map((cell: any, index: any) => (
+// 												<div
+// 													key={'cell_' + index}
+// 													className="flex w-full flex-wrap justify-start px-6 py-2"
+// 												>
+// 													{parseInt(cell) * quantity} ريال / مندوب / سنويا
+// 												</div>
+// 											))}
+// 										</div>
--- next/src/components/ui/lang-select.tsx
+++ next/src/components/ui/lang-select.tsx
@@ -28,31 +28,29 @@ const LangSelect = () => {
 	}
 
 	return (
-		<div>
-			<div className="flex items-center">
-				<InteractiveDetails className="group relative">
-					<summary className="group flex h-8 items-center gap-1 rounded-md px-3 text-cyan-950/80 no-underline transition-all hover:bg-teal-50 hover:text-cyan-700">
-						<PiGlobe />
-						{locale == 'ar' ? 'AR' : 'EN'}
-						<PiCaretRightBold className="size-3 translate-y-0 text-gray-500/50 transition-transform duration-300 group-open:rotate-90 group-hover:translate-y-[2px] md:rotate-90" />
-					</summary>
+		<div className="flex items-center">
+			<InteractiveDetails className="group relative">
+				<summary className="group flex h-8 items-center gap-1 rounded-md px-3 text-cyan-950/80 no-underline transition-all hover:bg-teal-50 hover:text-cyan-700">
+					<PiGlobe />
+					{locale == 'ar' ? 'AR' : 'EN'}
+					<PiCaretRightBold className="size-3 translate-y-0 text-gray-500/50 transition-transform duration-300 group-open:rotate-90 group-hover:translate-y-[2px] md:rotate-90" />
+				</summary>
 
-					<ul className="anim-fade-to-b top-full flex flex-col justify-between gap-1 rounded-lg border border-gray-100 bg-white p-3 shadow-md md:absolute">
-						<li
-							className="cursor-pointer px-4 py-2 text-cyan-950/80 hover:bg-teal-50 hover:text-teal-600"
-							onClick={() => onSelectChange('ar')}
-						>
-							Arabic
-						</li>
-						<li
-							className="cursor-pointer px-4 py-2 text-cyan-950/80 hover:bg-teal-50 hover:text-teal-600"
-							onClick={() => onSelectChange('en')}
-						>
-							English
-						</li>
-					</ul>
-				</InteractiveDetails>
-			</div>
+				<ul className="anim-fade-to-b top-full flex flex-col justify-between gap-1 rounded-lg border border-gray-100 bg-white p-3 shadow-md md:absolute">
+					<li
+						className="cursor-pointer px-4 py-2 text-cyan-950/80 hover:bg-teal-50 hover:text-teal-600"
+						onClick={() => onSelectChange('ar')}
+					>
+						Arabic
+					</li>
+					<li
+						className="cursor-pointer px-4 py-2 text-cyan-950/80 hover:bg-teal-50 hover:text-teal-600"
+						onClick={() => onSelectChange('en')}
+					>
+						English
+					</li>
+				</ul>
+			</InteractiveDetails>
 		</div>
 	)
 }
