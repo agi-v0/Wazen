6e277f116f775a0ed49df7f8f9e5e683af1f765f
omaro <omaroubari16@gmail.com>
2024-08-21 13:22:29 +0300

    fixed solutions benefits

--- next/src/ui/modules/Benefits.tsx
+++ next/src/ui/modules/Benefits.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { useMotionValueEvent, useScroll } from 'framer-motion'
 import { motion } from 'framer-motion'
 import Img from '@/ui/Img'
@@ -9,6 +9,7 @@ import {
 	PortableTextTypeComponentProps,
 } from 'next-sanity'
 import { cn } from '@/lib/utils'
+import { useMediaQuery } from '@/hooks/use-media-query'
 
 export default function Benefits({
 	benefits,
@@ -22,7 +23,7 @@ export default function Benefits({
 			block: ({ value }: PortableTextTypeComponentProps<any>) => {
 				if (value.style === 'h2') {
 					return (
-						<h2 className="h3 text-balance text-start font-semibold leading-tight text-cyan-950">
+						<h2 className="h2 text-balance text-start font-semibold leading-tight text-cyan-950">
 							{value.children.map((child: any) => child.text).join('')}
 						</h2>
 					)
@@ -67,36 +68,75 @@ export default function Benefits({
 
 	const [imageList, setImageList] = useState(images[0])
 
+	useEffect(() => {
+		setImageList(images[activeCard % images.length])
+	}, [activeCard])
+
+	const isDesktop = useMediaQuery('(min-width: 1280px)')
+	console.log('is desktop', isDesktop)
+
 	return (
 		<section
 			className={
 				'section fluid-vertical-space px-[var(--padding-horizontal--main)]'
 			}
 		>
-			<motion.div
-				className="fluid-gap hidden pb-12 xl:visible xl:flex"
-				ref={ref}
-			>
-				{/* <div className="sticky top-32 hidden h-full md:block">
-					<div className="flex h-80 flex-col items-center justify-between py-6">
-						{numbers.map((number, index) => (
-							<motion.div
-								initial={{
-									opacity: 0,
-								}}
-								animate={{
-									opacity: activeCard === index ? 1 : 0.3,
-								}}
-								className="text-large font-semibold text-cyan-950"
-								key={number + index}
-							>
-								{number}
-							</motion.div>
-						))}
-					</div>
-				</div> */}
+			{isDesktop ? (
+				<motion.div className="fluid-gap flex pb-12" ref={ref}>
+					{/* <div className="sticky top-32 hidden h-full md:block">
+				<div className="flex h-80 flex-col items-center justify-between py-6">
+					{numbers.map((number, index) => (
+						<motion.div
+							initial={{
+								opacity: 0,
+							}}
+							animate={{
+								opacity: activeCard === index ? 1 : 0.3,
+							}}
+							className="text-large font-semibold text-cyan-950"
+							key={number + index}
+						>
+							{number}
+						</motion.div>
+					))}
+				</div>
+			</div> */}
 
-				<div className="flex w-full flex-col items-start">
+					<div className="flex w-full flex-col items-start">
+						{content.map((item: any, index: any) => {
+							return (
+								<motion.div
+									initial={{
+										opacity: 0,
+									}}
+									animate={{
+										opacity: activeCard === index ? 1 : 0.3,
+									}}
+									className={cn(
+										'flex h-96 w-full flex-col justify-center space-y-6 py-6',
+										(index = 2 ? 'h-[446px]' : ''),
+									)}
+									key={item.content + index}
+								>
+									<PortableText value={item.content} components={components} />
+								</motion.div>
+							)
+						})}
+					</div>
+
+					<div className="sticky left-0 top-32 h-full w-full">
+						<Img
+							image={imageList}
+							imageWidth={640}
+							className="fade-out fade-in relative aspect-[4/3] h-auto w-full overflow-hidden rounded-2xl border-8 border-white object-cover shadow-md"
+						/>
+					</div>
+				</motion.div>
+			) : (
+				<motion.div
+					className="flex flex-col gap-6 px-[var(--padding-horizontal--main)] xl:hidden"
+					ref={ref}
+				>
 					{content.map((item: any, index: any) => {
 						return (
 							<motion.div
@@ -106,53 +146,21 @@ export default function Benefits({
 								animate={{
 									opacity: activeCard === index ? 1 : 0.3,
 								}}
-								className={cn(
-									'flex h-96 w-full flex-col justify-center space-y-6 py-6',
-									(index = 2 ? 'h-[446px]' : ''),
-								)}
+								className="fluid-vertical-space flex w-full flex-col items-center justify-center gap-6 text-center"
 								key={item.content + index}
 							>
 								<PortableText value={item.content} components={components} />
+								{/* <div className="relative flex items-center justify-center overflow-hidden rounded-lg border-8 border-white shadow-md"></div> */}
+								<Img
+									image={content?.[index].image}
+									imageWidth={640}
+									className="relative aspect-[4/3] h-auto w-full overflow-hidden rounded-2xl border-8 border-white object-cover shadow-md lg:max-w-[450px]"
+								/>
 							</motion.div>
 						)
 					})}
-				</div>
-
-				<div className="sticky left-0 top-32 h-full w-full">
-					<Img
-						image={imageList}
-						imageWidth={640}
-						className="relative aspect-[4/3] h-auto w-full overflow-hidden rounded-2xl border-8 border-white object-cover shadow-md"
-					/>
-				</div>
-			</motion.div>
-			<motion.div
-				className="flex flex-col gap-6 px-[var(--padding-horizontal--main)] xl:hidden"
-				ref={ref}
-			>
-				{content.map((item: any, index: any) => {
-					return (
-						<motion.div
-							initial={{
-								opacity: 0,
-							}}
-							animate={{
-								opacity: activeCard === index ? 1 : 0.3,
-							}}
-							className="fluid-vertical-space flex w-full flex-col items-center justify-center gap-6 text-center"
-							key={item.content + index}
-						>
-							<PortableText value={item.content} components={components} />
-							{/* <div className="relative flex items-center justify-center overflow-hidden rounded-lg border-8 border-white shadow-md"></div> */}
-							<Img
-								image={content?.[index].image}
-								imageWidth={640}
-								className="relative aspect-[4/3] h-auto w-full overflow-hidden rounded-2xl border-8 border-white object-cover shadow-md lg:max-w-[450px]"
-							/>
-						</motion.div>
-					)
-				})}
-			</motion.div>
+				</motion.div>
+			)}
 		</section>
 	)
 }
