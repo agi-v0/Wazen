370bd427db72f04cbe3425bd7f6a9d768f85876b
seifodev <seifodev@gmail.com>
2024-10-29 17:24:41 +0300

    trying to resolve i.c(arguments) intercom issue 2

--- next/src/components/intercom/IntercomProvider.tsx
+++ next/src/components/intercom/IntercomProvider.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 export default function IntercomProvider() {
     useEffect(() => {
-        window.intercomSettings = { app_id: "desatz83" };
+        window.intercomSettings = { app_id: "YOUR_APP_ID" };
         (function () {
             const w = window as any;
             const ic = w.Intercom;
@@ -12,7 +12,8 @@ export default function IntercomProvider() {
                 ic("update", w.intercomSettings);
             } else {
                 const d = document;
-                const i = function (...args: any[]) { (i.q = i.q || []).push(args); };
+                const i: any = function (...args: any[]) { (i.q = i.q || []).push(args); };
+                i.q = i.q || [];
                 w.Intercom = i;
                 const l = function () {
                     const s = d.createElement("script");
