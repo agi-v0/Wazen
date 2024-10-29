4513220560260ed3cfc1dab5f91ea2af67535d88
seifodev <seifodev@gmail.com>
2024-10-29 17:20:11 +0300

    trying to resolve i.c(arguments) intercom issue

--- next/src/components/intercom/IntercomProvider.tsx
+++ next/src/components/intercom/IntercomProvider.tsx
@@ -4,13 +4,31 @@ import { useEffect } from 'react';
 export default function IntercomProvider() {
     useEffect(() => {
         window.intercomSettings = { app_id: "desatz83" };
-        (function() {
-            var w = window; var ic = w.Intercom;
-            if (typeof ic === "function") { ic("reattach_activator"); ic("update", w.intercomSettings); }
-            else { var d = document; var i = function() { i.c(arguments); }; i.q = []; i.c = function(args) { i.q.push(args); };
-                w.Intercom = i; var l = function() { var s = d.createElement("script"); s.type = "text/javascript"; s.async = true;
-                    s.src = "https://widget.intercom.io/widget/YOUR_APP_ID"; var x = d.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x); };
-                if (document.readyState === "complete") { l(); } else if (w.attachEvent) { w.attachEvent("onload", l); } else { w.addEventListener("load", l, false); }
+        (function () {
+            const w = window as any;
+            const ic = w.Intercom;
+            if (typeof ic === "function") {
+                ic("reattach_activator");
+                ic("update", w.intercomSettings);
+            } else {
+                const d = document;
+                const i = function (...args: any[]) { (i.q = i.q || []).push(args); };
+                w.Intercom = i;
+                const l = function () {
+                    const s = d.createElement("script");
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = "https://widget.intercom.io/widget/YOUR_APP_ID";
+                    const x = d.getElementsByTagName("script")[0];
+                    x.parentNode.insertBefore(s, x);
+                };
+                if (document.readyState === "complete") {
+                    l();
+                } else if (w.attachEvent) {
+                    w.attachEvent("onload", l);
+                } else {
+                    w.addEventListener("load", l, false);
+                }
             }
         })();
     }, []);
