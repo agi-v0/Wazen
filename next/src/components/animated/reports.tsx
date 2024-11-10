407c70fdb11bb4a9dbd320ea0e3cc5ef916fa92d
omaro <omaroubari16@gmail.com>
2024-11-10 17:50:06 +0300

    x

--- next/src/components/animated/reports.tsx
+++ next/src/components/animated/reports.tsx
@@ -1,61 +1,67 @@
 'use client'
-import { m } from 'framer-motion'
+import { domAnimation, LazyMotion, m } from 'framer-motion'
 
 const reports = () => {
 	return (
-		<m.div
-			initial="hidden"
-			animate="visible"
-			variants={{
-				visible: {
-					transition: {
-						staggerChildren: 0.2,
-					},
-				},
-			}}
-			className="relative flex h-full max-w-md flex-row-reverse items-center gap-2"
-		>
-			<m.img
-				src="/statistic-img-1.svg"
-				alt="reports"
-				width={255.88}
-				height={338.44}
-				loading="lazy"
+		<LazyMotion features={domAnimation}>
+			<m.div
+				initial="hidden"
+				animate="visible"
 				variants={{
-					hidden: { opacity: 0, scale: 0.75, y: -20 },
 					visible: {
-						opacity: 1,
-						scale: 1,
-						y: 0,
+						transition: {
+							staggerChildren: 0.2,
+						},
 					},
 				}}
-				transition={{
-					repeat: Infinity,
-					repeatType: 'mirror',
-					repeatDelay: 1,
-				}}
-				className="h-auto w-full max-w-[50%] flex-shrink basis-1/2 rounded-lg shadow-lg"
-				style={{ rotate: '-6deg' }} // {{ edit_1 }}
-			/>
-			<m.img
-				src="/statistic-img-2.svg"
-				alt="reports"
-				width={255.88}
-				height={338.44}
-				loading="lazy"
-				variants={{
-					hidden: { opacity: 0, scale: 0.75, y: -20 },
-					visible: {
-						opacity: 1,
-						scale: 1,
-						y: 0,
-					},
-				}}
-				transition={{ repeatType: 'mirror', repeat: Infinity, repeatDelay: 1 }}
-				className="h-auto w-full max-w-[50%] flex-shrink basis-1/2 rounded-lg shadow-lg"
-				style={{ rotate: '6deg' }} // {{ edit_2 }}
-			/>
-		</m.div>
+				className="relative flex h-full max-w-md flex-row-reverse items-center gap-2"
+			>
+				<m.img
+					src="/statistic-img-1.svg"
+					alt="reports"
+					width={255.88}
+					height={338.44}
+					loading="lazy"
+					variants={{
+						hidden: { opacity: 0, scale: 0.75, y: -20 },
+						visible: {
+							opacity: 1,
+							scale: 1,
+							y: 0,
+						},
+					}}
+					transition={{
+						repeat: Infinity,
+						repeatType: 'mirror',
+						repeatDelay: 1,
+					}}
+					className="h-auto w-full max-w-[50%] flex-shrink basis-1/2 rounded-lg shadow-lg"
+					style={{ rotate: '-6deg' }} // {{ edit_1 }}
+				/>
+				<m.img
+					src="/statistic-img-2.svg"
+					alt="reports"
+					width={255.88}
+					height={338.44}
+					loading="lazy"
+					variants={{
+						hidden: { opacity: 0, scale: 0.75, y: -20 },
+						visible: {
+							opacity: 1,
+							scale: 1,
+							y: 0,
+						},
+					}}
+					transition={{
+						repeatType: 'mirror',
+						repeat: Infinity,
+						repeatDelay: 1,
+					}}
+					className="h-auto w-full max-w-[50%] flex-shrink basis-1/2 rounded-lg shadow-lg"
+					style={{ rotate: '6deg' }} // {{ edit_2 }}
+				/>
+			</m.div>
+		</LazyMotion>
 	)
 }
 
