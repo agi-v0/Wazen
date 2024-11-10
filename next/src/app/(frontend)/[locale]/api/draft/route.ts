22422a7f4fa50708a78c52cf94c65960d35aa7cd
omaro <omaroubari16@gmail.com>
2024-11-10 13:00:53 +0300

    x

--- next/src/app/(frontend)/[locale]/api/draft/route.ts
+++ next/src/app/(frontend)/[locale]/api/draft/route.ts
@@ -1,23 +1,7 @@
+import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import client from '@/lib/sanity/client'
-import { validatePreviewUrl } from '@sanity/preview-url-secret'
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { token } from '@/lib/sanity/token'
 
-const clientWithToken = client.withConfig({
-	token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+export const { GET } = defineEnableDraftMode({
+	client: client.withConfig({ token }),
 })
-
-export async function GET(request: Request) {
-	const { isValid, redirectTo = '/' } = await validatePreviewUrl(
-		clientWithToken,
-		request.url,
-	)
-
-	if (!isValid) {
-		return new Response('Invalid secret', { status: 401 })
-	}
-
-	draftMode().enable()
-
-	redirect(redirectTo)
-}
