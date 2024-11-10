fef193a36b71e8606e56cbb374048250ee86ada5
omaro <omaroubari16@gmail.com>
2024-11-10 15:57:12 +0300

    x

--- next/src/components/ui/modules/RichtextModule/AnchoredHeading.tsx
+++ next/src/components/ui/modules/RichtextModule/AnchoredHeading.tsx
@@ -1,20 +1,22 @@
 import { slug } from '@/lib/utils'
 import type { PortableTextBlock, PortableTextComponentProps } from 'next-sanity'
-import { ReactNode } from 'react'
 
 export default function AnchoredHeading({
+	as: Tag,
 	children,
 	value,
-}: ReactNode & PortableTextComponentProps<PortableTextBlock>) {
+}: {
+	as: React.ElementType
+} & PortableTextComponentProps<PortableTextBlock>) {
 	const id = slug(value.children[0].text)
 
 	return (
-		<span id={id} className="group">
+		<Tag id={id} className="group">
 			{children}
 
 			<a className="ms-2 md:hidden md:group-hover:inline-block" href={`#${id}`}>
 				🔗
 			</a>
-		</span>
+		</Tag>
 	)
 }
