d6b84a625c38d2ceb11dd3ac47de5f3516cf3470
omaro <omaroubari16@gmail.com>
2024-12-14 13:17:09 +0300

    fix og image font

--- next/sanity/plugins/sanity-plugin-advanced-array/index.ts
+++ next/sanity/plugins/sanity-plugin-advanced-array/index.ts
@@ -1,6 +1,10 @@
-import {definePlugin, isArrayOfObjectsInputProps, isObjectItemProps} from 'sanity'
-import {AdvancedArrayInput} from './components/AdvancedArrayInput'
-import {AdvancedArrayItem} from './components/AdvancedArrayItem'
+import {
+	definePlugin,
+	isArrayOfObjectsInputProps,
+	isObjectItemProps,
+} from 'sanity'
+import { AdvancedArrayInput } from './components/AdvancedArrayInput'
+import { AdvancedArrayItem } from './components/AdvancedArrayItem'
 
 /**
  * Usage in `sanity.config.ts` (or .js)
@@ -19,38 +23,38 @@ import {AdvancedArrayItem} from './components/AdvancedArrayItem'
  * ```
  */
 export const advancedArray = definePlugin(() => {
-  return {
-    name: 'sanity-plugin-advanced-array',
+	return {
+		name: 'sanity-plugin-advanced-array',
 
-    form: {
-      components: {
-        input: (props) => {
-          if (
-            isArrayOfObjectsInputProps(props) &&
-            // @ts-expect-error
-            props.schemaType.options?.advanced
-          ) {
-            return AdvancedArrayInput(props)
-          }
+		form: {
+			components: {
+				input: (props) => {
+					if (
+						isArrayOfObjectsInputProps(props) &&
+						// @ts-expect-error
+						props.schemaType.options?.advanced
+					) {
+						return AdvancedArrayInput(props)
+					}
 
-          return props.renderDefault(props)
-        },
-        item: (props) => {
-          if (
-            isObjectItemProps(props) &&
-            // @ts-expect-error
-            props.parentSchemaType.options?.advanced &&
-            // @ts-expect-error
-            (props.parentSchemaType.options.advanced.inline !== 'off' ||
-              // @ts-expect-error
-              props.parentSchemaType.options.advanced.select)
-          ) {
-            return AdvancedArrayItem(props)
-          }
+					return props.renderDefault(props)
+				},
+				item: (props) => {
+					if (
+						isObjectItemProps(props) &&
+						// @ts-expect-error
+						props.parentSchemaType.options?.advanced &&
+						// @ts-expect-error
+						(props.parentSchemaType.options.advanced.inline !== 'off' ||
+							// @ts-expect-error
+							props.parentSchemaType.options.advanced.select)
+					) {
+						return AdvancedArrayItem(props)
+					}
 
-          return props.renderDefault(props)
-        },
-      },
-    },
-  }
+					return props.renderDefault(props)
+				},
+			},
+		},
+	}
 })
