a771bc1c83f1d3097be617792bdd026a3bd5cdf0
omaro <omaroubari16@gmail.com>
2025-03-28 17:26:59 +0300

    x

--- sanity/plugins/sanity-plugin-generate-og-image/src/Editor.tsx
+++ sanity/plugins/sanity-plugin-generate-og-image/src/Editor.tsx
@@ -37,7 +37,7 @@ const Editor: React.FC<EditorProps> = (props) => {
 	const fields = activeLayout.fields || []
 	const width = activeLayout.dimensions?.width || DEFAULT_DIMENSIONS.width
 	const height = activeLayout.dimensions?.height || DEFAULT_DIMENSIONS.height
-	console.log(data)
+
 	return (
 		<Card
 			scheme={scheme}
