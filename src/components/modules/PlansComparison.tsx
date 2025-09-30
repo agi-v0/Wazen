fa76b3c633f92702113c2dae9e11f0006b972914
agi-v0 <omaroubari16@gmail.com>
2025-09-30 22:17:57 +0300

    pricing comparison table

--- src/components/modules/PlansComparison.tsx
+++ src/components/modules/PlansComparison.tsx
@@ -6,6 +6,7 @@ import { Switch } from '@/components/ui/switch'
 import { cn } from '@/lib/utils'
 import { Icon } from '@iconify-icon/react'
 import { set2 } from '@/components/portable-text'
+import SaudiRiyal from '../saudi-riyal'
 
 const PlansComparison = ({
 	content,
@@ -18,7 +19,7 @@ const PlansComparison = ({
 }>) => {
 	const switchLabel =
 		locale === 'en' ? 'Yearly (two months free)' : 'سنوي (شهرين مجاناً)'
-	const SR = locale === 'en' ? 'SR' : 'ريال'
+	const SR = <SaudiRiyal className="text-gray-950" />
 	const monthly = locale === 'en' ? 'monthly' : 'شهرياً'
 	const yearly = locale === 'en' ? 'Yearly' : 'سنوياً'
 
@@ -29,12 +30,12 @@ const PlansComparison = ({
 			<div className="fluid-gap flex w-full flex-col items-center">
 				<PortableText value={content} components={set2} />
 
-				<div className="container">
+				<div className="w-full">
 					{/* <!-- table header (prices) --> */}
 
 					<div className="sticky top-(--header-height) z-2 flex w-full flex-row items-end justify-between bg-white *:w-full max-lg:hidden max-lg:w-fit *:max-lg:w-48">
 						<div className="flex flex-col items-start justify-end px-2 py-4">
-							<div className="flex h-10 w-full flex-row items-center justify-center gap-4 text-sm font-medium text-gray-400 rtl:flex-row-reverse">
+							<div className="flex h-10 w-full flex-row items-center justify-end gap-4 text-sm font-medium text-gray-400 rtl:flex-row-reverse">
 								{switchLabel}
 								<Switch checked={isYearly} onCheckedChange={setIsYearly} />
 							</div>
@@ -52,16 +53,16 @@ const PlansComparison = ({
 											</h3>
 											{/* <p className="text-base text-gray-600">description</p> */}
 										</div>
-										<p className="h5 inline-flex flex-row items-end gap-1 pt-1 font-semibold tracking-normal text-gray-950 ltr:items-center">
-											<span className="amount">
-												{' '}
-												{isYearly ? row.cells[0] : row.cells[1]}
-											</span>
-											{SR}
-											<span className="text-large font-medium text-gray-400">
-												{' '}
-												/{isYearly ? yearly : monthly}
-											</span>
+										<p className="h5 inline-flex flex-row items-center gap-1 pt-1 font-semibold tracking-normal text-gray-950 ltr:items-center">
+											{isYearly ? row.cells[0] : row.cells[1]}
+											{!isNaN(parseInt(row.cells[0])) && (
+												<>
+													{SR}
+													<span className="text-large font-medium text-gray-400">
+														/{isYearly ? yearly : monthly}
+													</span>
+												</>
+											)}
 										</p>
 										{/* <button
 											className={cn(
@@ -79,24 +80,28 @@ const PlansComparison = ({
 						)}
 					</div>
 					{/* table details */}
-					<div className="relative mt-6 flex w-full flex-col gap-9 text-sm">
+					<div className="relative mt-6 flex w-full flex-col gap-9 overflow-x-clip text-sm">
 						{details?.slice(1).map((detail: any, index: any) => (
 							<div key={'details_' + index} id={'details_' + index}>
 								{/* table section header */}
 								<h3
-									className="text-main sticky top-[calc(var(--header-height)+106.31px)] z-1 flex w-full flex-col justify-end rounded-2xl bg-teal-100 p-(--text-main--font-size) font-semibold text-gray-900 max-lg:top-(--header-height)"
+									className="text-main sticky top-[calc(var(--header-height)+106.31px)] z-1 flex w-full flex-col justify-end rounded-2xl bg-teal-100 p-(--text-main--font-size) py-3 font-semibold text-cyan-900 max-lg:top-(--header-height)"
 									aria-hidden="true"
 								>
 									{detail.title}
 								</h3>
 								<div className="relative w-full max-lg:overflow-x-scroll">
 									{/* table section details */}
-									<div className="w-fit text-gray-600 lg:w-full">
-										<div className="flex flex-row-reverse justify-end text-start text-sm font-medium text-gray-500 *:w-48 lg:hidden rtl:flex-row rtl:items-end">
+									<div className="w-full text-gray-600 lg:w-full">
+										<div className="flex flex-row-reverse justify-end border-b text-start text-sm font-medium text-gray-500 *:w-full lg:hidden rtl:flex-row rtl:items-end">
+											<div className="w-full"></div>
 											{details[0].specs.rows.map(
 												(row: { cells: string[] }, index: string) => {
 													return (
-														<div key={index} className="px-6 py-3">
+														<div
+															key={index}
+															className="w-full p-2 py-3 lg:px-4"
+														>
 															{row.cells[2]}
 														</div>
 													)
@@ -106,7 +111,7 @@ const PlansComparison = ({
 										{detail.specs?.rows?.map((row: any, index: any) => (
 											<div
 												key={'rows_' + index}
-												className="flex flex-row-reverse justify-between border-b border-gray-200 text-start *:w-full *:max-lg:w-48"
+												className="flex flex-row-reverse justify-between border-b border-gray-200 text-start"
 											>
 												{row.cells.map((cell: string, index: any) => {
 													const cleanedCell = cell
@@ -117,7 +122,7 @@ const PlansComparison = ({
 																	key={'cell_' + index}
 																	id={'cell_' + index}
 																	className={cn(
-																		'flex items-center justify-start px-6 py-3',
+																		'flex w-full items-center justify-start p-2 py-3 lg:px-4',
 																		index == 1 ? 'bg-teal-50' : '',
 																	)}
 																>
@@ -132,7 +137,7 @@ const PlansComparison = ({
 																<div
 																	key={'cell_' + index}
 																	className={cn(
-																		'flex justify-start px-6 py-2 text-sm text-teal-500',
+																		'flex w-full items-center justify-start p-2 py-3 text-sm text-teal-500 lg:px-4',
 																		index == 1 ? 'bg-teal-50' : '',
 																	)}
 																>
@@ -147,11 +152,10 @@ const PlansComparison = ({
 																<div
 																	key={'cell_' + index}
 																	className={cn(
-																		'flex items-center justify-start px-6 py-3',
-
+																		'flex basis-full items-center justify-start p-2 py-3 lg:px-4',
 																		index == 1 ? 'bg-teal-50' : '',
 																		index == 3
-																			? 'sticky end-0 w-48 bg-white font-medium'
+																			? 'w-full bg-white font-medium'
 																			: '',
 																	)}
 																>
