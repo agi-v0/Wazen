6d044b98441779618d51e492453808db31812f28
agi-v0 <omaroubari16@gmail.com>
2025-10-08 17:35:44 +0300

    use next/font/google for fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,4 +1,5 @@
-import localfont from 'next/font/local'
+// import localfont from 'next/font/local'
+import { Rubik } from 'next/font/google'
 
 // export const inter = Inter({
 // 	subsets: ['latin'],
@@ -7,16 +8,23 @@ import localfont from 'next/font/local'
 // 	variable: '--font-inter',
 // })
 
-export const rubik = localfont({
-	// src: '../../public/fonts/Rubik-VariableFont_wght.woff2',
-	src: [
-		{
-			path: '../../public/fonts/rubik-v31-arabic-variable.woff2',
-		},
-		{
-			path: '../../public/fonts/rubik-v31-latin-variable.woff2',
-		},
-	],
+// export const rubik = localfont({
+// 	// src: '../../public/fonts/Rubik-VariableFont_wght.woff2',
+// 	src: [
+// 		{
+// 			path: '../../public/fonts/rubik-v31-arabic-variable.woff2',
+// 		},
+// 		{
+// 			path: '../../public/fonts/rubik-v31-latin-variable.woff2',
+// 		},
+// 	],
+// 	display: 'swap',
+// 	variable: '--font-inter',
+// })
+
+export const rubik = Rubik({
+	subsets: ['latin', 'arabic'],
 	display: 'swap',
+	weight: ['400', '500', '600', '700'],
 	variable: '--font-inter',
 })
