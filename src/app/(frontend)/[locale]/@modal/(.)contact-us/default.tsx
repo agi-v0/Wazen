f8a42345f8c9f1e76318501aafd5c7243a4ca9c4
agi-v0 <omaroubari16@gmail.com>
2025-10-12 18:47:09 +0300

    update en dictionary and default contact-us parallel route page

--- src/app/(frontend)/[locale]/@modal/(.)contact-us/default.tsx
+++ src/app/(frontend)/[locale]/@modal/(.)contact-us/default.tsx
@@ -1,3 +1,61 @@
-export default function Default() {
-	return null
+import Modules from '@/components/modules'
+import { fetchSanityLive } from '@/sanity/lib/fetch'
+import groq from 'groq'
+
+type Props = {
+	params: Promise<{ locale: 'en' | 'ar' }>
+}
+
+export default async function Default({ params }: Props) {
+	const resolvedParams = await params
+
+	const page = await getPage(resolvedParams.locale)
+	return <Modules modules={page?.modules} locale={resolvedParams.locale} />
+}
+
+async function getPage(locale: 'en' | 'ar') {
+	return await fetchSanityLive({
+		query: groq`*[_type == 'page' && metadata.slug.current == "contact-us" && language == '${locale}'][0]{
+			...,
+			modules[]{
+				...,
+				ctas[]{
+					...,
+					link{
+						...,
+						internal->{ title, metadata }
+					}
+				},
+				products[]{
+					...,
+						link{
+							...,
+							internal->{ title, metadata },
+					}
+				},
+				_type == 'faq-list' => {
+          sideNote {
+            ...,
+            link {
+              ...,
+              internal->{ title, metadata }
+            }
+          }
+        },
+				categories[]->{title, slug},
+				logos[]->,
+				plans[]->,
+				partnerslogos[]->,
+				testimonials[]->,
+				items[]->,
+
+			},
+			metadata {
+				...,
+				'ogimage': image.asset->url
+			}
+		}`,
+		params: {},
+		tags: ['page'],
+	})
 }
